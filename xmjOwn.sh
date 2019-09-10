8aca56353ce62f1325cd44bd582315f8401888c3
valorad <valorad@outlook.com>
2019-09-09 20:38:31 -0400

    xmj-own

---
+++ xmjOwn.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+folder=$1
+owner=$2
+group=$3
+permission=$4
+
+: ${folder:="."}
+: ${owner:="valorad"}
+: ${group:="xmj-alliance"}
+: ${permission:=755}
+
+xmjOwn() {
+  sudo chown $owner -R $folder
+  sudo chgrp $group -R $folder
+  sudo chmod $permission -R $folder
+}
+
+xmjOwn
