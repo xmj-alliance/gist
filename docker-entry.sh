9b5317e0790e98b14d0f746aab885e0689695118
Valorad the Oneiroseeker <11241413+valorad@users.noreply.github.com>
2019-10-08 20:07:58 -0400

    Update docker-entry.sh

--- docker-entry.sh
+++ docker-entry.sh
@@ -4,10 +4,16 @@
 username=$EXEC_USER
 userid=${EXEC_USER_ID}
 
+folder=$1
+permission=$2
+
+: ${folder:="."}
+: ${permission:=755}
+
 echo "Summoning $username - UID:$userid ..."
 
 # Add local user
 adduser $username -u $userid -D -s /bin/sh
-chown -R $username /www
-chmod -R 755 /www
+chown -R $username $folder
+chmod -R $permission $folder
 exec su-exec $username "$@"
